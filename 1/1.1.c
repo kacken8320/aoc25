59340b17b183d00d06b92aeb77f2fed09d32a4e3
kacken8320 <108149707+kacken8320@users.noreply.github.com>
2025-12-07 00:19:26 +0100

    Add comments and formatting to 1.1.c

--- 1/1.1.c
+++ 1/1.1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 /********************************/
 /*				*/
 /* L -> minus			*/
@@ -10,6 +11,7 @@
 /* 	calculation equals 0	*/
 /*				*/
 /********************************/
+
 
 int main() {
 	int sum = 50;
